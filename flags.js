4e830af2476ea6e8335faa468865aa1912fe4ec6
Keith Talbot <7109623+mad-science-lab@users.noreply.github.com>
2023-08-10 13:56:04 -0400

    Update flags.js

--- flags.js
+++ flags.js
@@ -1,3 +1,5 @@
+//Use to change the color of an object based on the active Flag in iRacing
+
 var Flag = $prop('DataCorePlugin.GameData.Flag_Name');
 
 switch (Flag){
@@ -5,20 +7,20 @@ switch (Flag){
 		return '#7DFFFF00';
 		break;
 	case 'Green':
-		return '#7DFFFF00';
+		return '#8000CC00';
 		break;
 	case 'Black':
-		return '#7D2D0060';
+		return '#7D2D0060';  //Dark Purple
 		break;
 	case 'Blue':
 		return '#7D0000FF';
 		break;
 	case 'Checkered':
-		return '#00FFFFFF';
+		return '#00FFFFFF'; //Transparent
 		break;
 	case 'White':
 		return '#7DFFFFFF';
 		break;
 	default:
-		return '#00FFFFFF';	
+		return '#00FFFFFF'; //Transparent
 }
